c41a0707c108c060ea5869a789ede72c0fbaf085
Hriday Agrawal <hriday.agrawal03@gmail.com>
2023-01-05 20:10:44 +0530

    commit

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.homeRoutes = (req, res) => {
   axios
-    .get("https://usermanagementapp.onrender.com/api/users")
+    .get("http://localhost:3000/api/users")
     .then(function (response) {
       res.render("index", { users: response.data });
     })
@@ -16,7 +16,7 @@ exports.homeRoutes = (req, res) => {
 
 exports.update_user = (req, res) => {
   axios
-    .get("https://usermanagementapp.onrender.com/api/users", {
+    .get("http://localhost:3000/api/users", {
       params: { id: req.query.id },
     })
     .then(function (userData) {
